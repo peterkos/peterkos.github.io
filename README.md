fde0b22cbafce2d2d31a8ffb58e5c278927f460e
Peter Kos <pkos91@icloud.com>
2014-10-08 08:51:50 -0400

    Wiped & Restarted w/ Gulp
    Reset repository
    Added Gulp
    Working on new design

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 ##Hello!
 
 This is the repository for all of the files at [peterkos.github.io](peterkos.github.io). 
-
-I'll also be using this repository to test out some features of Git and Github that might be overkill (for instance, creating a branch for editing one line of the readme).
-
-
-
---
+++ gulp/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp         = require('gulp');
+var rename       = require('gulp-rename');
+var browserSync  = require('browser-sync');
+var reload       = browserSync.reload;
+// var notify       = require('gulp-notify');
+var sass         = require('gulp-sass');
+var minifycss    = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "../"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+  return gulp.src('../scss/styles.scss')
+    //Parse
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 4 versions'))
+    .pipe(gulp.dest('../css'))
+    //Minify 
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('../css'))
+    //Reload
+    .pipe(reload({stream:true}));
+    // .pipe(notify({ message: 'Styles task complete' }));
+});
+
+// Default task to be run with gulp
+gulp.task('default', ['styles', 'browser-sync'], function () {
+    gulp.watch("../scss/*.scss", ['styles']);
+
+});
