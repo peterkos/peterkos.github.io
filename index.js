0b427da03a8f1f3799a15f2e203d1e9a0dc7518f
Peter Kos <pkos91@icloud.com>
2019-07-27 18:00:54 -0700

    Added typeit effect to .info

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("asdf")
+
+
+import TypeIt from "typeit"
+
+
+
+// The fun typewriter effect!
+// It sucks that each element is <b> but that's the best way I found to do it for now.
+// CSS would probably be better but oh well.
+new TypeIt("#typeit", {
+	strings: ["<b>teaching</b>.", 
+	"<b>coding</b>.", 
+	"<b>designing</b>.", 
+	"<b>composing</b>.", 
+	"<b>using the cowboy emoji unironically</b>.", 
+	"<b>watching linustechtips</b>.", 
+	"<b>yelling into the void</b>.", 
+	"<b>twittereeting</b>."],
+	speed: 200,
+	cursorSpeed: 1000,
+	nextStringDelay: 3000,
+	html: true,
+	cursor: true,
+	breakLines: false,
+	startDelay: 500,
+	loopDelay: 10,
+	loop: true,
+	waitUntilVisible: true
+}).go();
+
